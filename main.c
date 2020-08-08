9fc0c01425aa9468010b1543529b1d469ec46510
DESKTOP-I8DK67B\Riton <ritonelion@outlook.com>
2020-08-08 20:54:06 +0800

    语素为单字符的加减中缀表达式转后缀表达式

--- main.c
+++ main.c
@@ -3,9 +3,62 @@
 //
 
 #include <stdio.h>
+//#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_SIZE 4096
+
+void match(char **);
+void expr(char **);
+void rest(char **);
+void term(char **);
+
+void match(char **lookaheadPos) {
+    (*lookaheadPos)++;
+}
+
+void expr(char **lookahead) {
+    term(lookahead);
+    rest(lookahead);
+}
+
+void rest(char **lookahead) {
+    if (**lookahead == '+') {
+        match(lookahead);
+        term(lookahead);
+        printf("+ ");
+        rest(lookahead);
+    } else if (**lookahead == '-') {
+        match(lookahead);
+        term(lookahead);
+        printf("- ");
+        rest(lookahead);
+    } else {}
+}
+
+void term(char **lookahead) {
+    if ( **lookahead > '0' && **lookahead <= '9') {
+        char t = **lookahead;
+        match(lookahead);
+        printf("%c ", t);
+    } else {
+        printf("syntax error");
+        exit(1);
+    }
+}
+
+
 
 int main() {
-    printf("hello world!\n");
+    char *inputBuffer = calloc(sizeof(char), MAX_INPUT_SIZE);
+    char *lookahead = inputBuffer;
+
+    scanf ("%s", inputBuffer);
+//    strcpy(inputBuffer, "9+6-7");
+//    printf("%s\n", inputBuffer);
+    expr(&lookahead);
+    free(inputBuffer);
 
     return 0;
 }
